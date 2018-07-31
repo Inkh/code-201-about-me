eebbb1e256c963f9e0a2d7b01885a198c98cae78
Jeff <jhwe92@gmail.com>
2018-07-31 14:14:47 -0700

    Added sorting hat. Reverted lint file indentation back to default

--- js/app.js
+++ js/app.js
@@ -1,19 +1,40 @@
 'use strict';
 
-
-console.log('hai');
+//Declare user score for later use
+let score = 0;
 
 //First Test question for user
-function userTest(){
-    let userResponse = prompt('Do you like cuttlefish? Answer Yes or No.').toLowerCase();
-    if (userResponse === 'yes'){
-        console.log('well of course!');
-        alert('Correct! You\'re awesome. Let\'s continue to the real thing now!');
-        document.getElementById('userContinue').innerHTML = 'Booyah!';
-    } else if(userResponse === 'no'){
-        console.log('bah, you suck');
-    } else{
-        console.log('please insert a valid response');
-    }
-}
+const userTest = () => {
+  let userResponse = prompt('Do you like cuttlefish? Answer Yes or No.').toLowerCase();
+  if (userResponse === 'yes'){
+    console.log(`User answers ${userResponse.toUpperCase()}! They're cool!`);
+    alert('Correct! You\'re awesome. Let\'s continue to the real thing now!');
+    document.getElementById('sortingHat').innerHTML = 'Just kidding. One more thing! Let us choose your school first!';
+    document.getElementById('userSort').innerHTML = '<button onClick="sortUser()">Sort now!</button>';
 
+  } else if (userResponse === 'no'){
+    console.log(`User answers ${userResponse.toUpperCase()}... They won't go far.`);
+    alert('What!? No!? Abort! Abort! You are not worthy to continue!');
+  } else{
+    console.log(`User answers with ${userResponse}... I don't even..`);
+    alert('Really? You can\'t follow some simple instruction!? Start over!');
+  }
+};
+
+
+const sortUser = () => {
+  let userSchool = prompt('Do you belong to Gryffindor, Slytherin, Hufflepuff, or Ravenclaw?').toLowerCase();
+
+  //Capitalize first letter in string for more formal look
+  let userSchoolCap = userSchool.charAt(0).toUpperCase() + userSchool.slice(1);
+
+  //Check user input for desired school. Add point accordingly.
+  if (userSchool !== 'ravenclaw' || userSchool !== 'hufflepuff' || userSchool !== 'slytherin' || userSchool !== 'gryffindor'){
+    alert(`Ooo...kay..? I guess 1 point to ${userSchoolCap}`);
+    score += 1;
+  } else{
+    alert(`Wooo! 50 points to ${userSchoolCap}!`);
+    score += 50;
+  }
+  console.log(`User score is now ${score}`);
+};
