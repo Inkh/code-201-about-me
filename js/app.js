a1d9e5a69650858187859151f9a987ed828c8e11
Jeff <jhwe92@gmail.com>
2018-07-31 13:28:33 -0700

    Added test question. Changed indent in lint file from 2 to 4

--- js/app.js
+++ js/app.js
@@ -2,3 +2,18 @@
 
 
 console.log('hai');
+
+//First Test question for user
+function userTest(){
+    let userResponse = prompt('Do you like cuttlefish? Answer Yes or No.').toLowerCase();
+    if (userResponse === 'yes'){
+        console.log('well of course!');
+        alert('Correct! You\'re awesome. Let\'s continue to the real thing now!');
+        document.getElementById('userContinue').innerHTML = 'Booyah!';
+    } else if(userResponse === 'no'){
+        console.log('bah, you suck');
+    } else{
+        console.log('please insert a valid response');
+    }
+}
+
